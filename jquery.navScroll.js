79c0285c067be2e5804062a4760b27e9eb21e994
leolux <mathias4u@gmx.net>
2016-06-22 15:12:40 +0200

    fix error in l.124

--- jquery.navScroll.js
+++ jquery.navScroll.js
@@ -121,7 +121,7 @@
 
       for (i = 0; l > i; i++) {
         var item = scrollItems[i];
-        if (item !== undefined) {
+        if (item !== undefined && typeof item.offset === 'function') {
           if (scrollPos > (item.offset().top - changeBounds)) {
             if (options.activeParent) {
               navItem.parent().removeClass(options.activeClassName);
